7abfe2970bcd221aef089f8db52aa80ef41a498b
v1n3l <aquinovinel@gmail.com>
2022-11-25 16:34:20 +0800

    my first commit

---
+++ dataset.php
@@ -0,0 +1,88 @@
+<?php
+    $arrProducts = array(
+        array(
+            'name'          => "Plain Home&Outdoor Rubber Slippers",
+            'description'   => "Home&Outdoor Rubber Slippers",
+            'price'         => "50",
+            'photo1'        => "product1.png",
+            'photo2'        => "product2.png"
+        ),
+        array(
+            'name'          => "Mostwear Korean Sport Slippers",
+            'description'   => "Mostwear Korean Sport Slippers For Men#911",
+            'price'         => "110",
+            'photo1'        => "product2b.png",
+            'photo2'        => "product2a.png",
+        ),
+        array(
+            'name'          => "Bestseller classic",
+            'description'   => "Bestseller classic non slip flip flops for men and women fashion sandals unisex sale",
+            'price'         => "200",
+            'photo1'        => "product3a.png",
+            'photo2'        => "product3b.png",
+        ),
+        array(
+            'name'          => "JEIKY. Men's MOSAIC",
+            'description'   => "JEIKY. Men's MOSAIC Black and White Trendy Flip Flops Rubber Slippers",
+            'price'         => "150",
+            'photo1'        => "product4a.png",
+            'photo2'        => "product4b.png",
+        ),
+        array(
+            'name'          => "Bossgo Korean Sport Rubber",
+            'description'   => "Bossgo Korean Sport Rubber Slippers For Men",
+            'price'         => "200",
+            'photo1'        => "product5a.png",
+            'photo2'        => "product5b.png",
+        ),
+        array(
+            'name'          => "COD Casual Slippers",
+            'description'   => "COD Casual Slippers for Men's & Women's (OEM-PREMIUM QUALITY)",
+            'price'         => "170",
+            'photo1'        => "product6a.png",
+            'photo2'        => "product6b.png",
+        ),
+        array(
+            'name'          => "JY. Men's NEW YORK Rubber Slippers",
+            'description'   => "JY. Men's NEW YORK Rubber Slippers Trendy Flip Flops #ZZ15 (Add One Size) TB",
+            'price'         => "150",
+            'photo1'        => "product7a.png",
+            'photo2'        => "product7b.png",
+        ), 
+        array(
+            'name'          => "Sport Slip-ons Slippers",
+            'description'   => "Sport Slip-ons Slippers for Women’s and men’s unisex (add one size)",
+            'price'         => "140",
+            'photo1'        => "product8a.png",
+            'photo2'        => "product8b.png",
+        ),
+        array(
+            'name'          => "classic A.fashion slip",
+            'description'   => "classic A.fashion slip on slippers for adults unisex mens at womens",
+            'price'         => "79",
+            'photo1'        => "product9a.png",
+            'photo2'        => "product9a.png",
+        ),
+        array(
+            'name'          => "Footwear PAPILO",
+            'description'   => "Slipper for Men's Original Footwear PAPILO (Made in THAILAND)",
+            'price'         => "80",
+            'photo1'        => "product10c.png",
+            'photo2'        => "product10d.png",
+        ),
+        array(
+            'name'          => "hai classic Nanyang & Shark star elephant slippers",
+            'description'   => "Flip Flops♀۩Thai classic Nanyang & Shark star elephant slippers natural rubber slippers for men",
+            'price'         => "160",
+            'photo1'        => "product11d.png",
+            'photo2'        => "product11c.png",
+        ),
+        array(
+            'name'          => "rendy flip flops",
+            'description'   => "✨Good quality✨ Trendy flip flops for men , slippers for men (A1956)",
+            'price'         => "200",
+            'photo1'        => "product12c.png",
+            'photo2'        => "product12d.png",
+        )
+    );
+?>
